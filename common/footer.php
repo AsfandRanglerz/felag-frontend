ee5527ed6629328a2bc0426c477fb0d0e7e53ce4
AsfandRanglerz <asfand.ranglerz@gmail.com>
2021-08-27 11:32:57 +0500

    changes

---
+++ common/footer.php
@@ -0,0 +1,119 @@
+        <footer class="footer">
+            <div class="footer-inner-upper">
+                <div class="footer-logo">
+<!--                    <img src="images/footer/footer-logo.svg" />-->
+                    <span class="footer-logo-text">Logo</span>
+                </div>
+                <div>
+                    <h5>Navigation</h5>
+                    <ul class="mb-0 list-style-none">
+                        <li><a href="#" class="link">See all features</a></li>
+                        <li><a href="#" class="link">Prices</a></li>
+                        <li><a href="#" class="link">Support</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Our solutions</h5>
+                    <ul class="mb-0 list-style-none">
+                        <li><a href="#" class="link">Management system</a></li>
+                        <li><a href="#" class="link">Digital lease</a></li>
+                        <li><a href="#" class="link">Collection of rent</a></li>
+                        <li><a href="#" class="link">Deposit account and other security</a></li>
+                        <li><a href="#" class="link">Rent guarantee from Aprila Bank</a></li>
+                        <li><a href="#" class="link">Occupancy protocol and relocation protocol</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Resources</h5>
+                    <ul class="mb-0 list-style-none">
+                        <li><a href="#" class="link">front page</a></li>
+                        <li><a href="#" class="link">sign in</a></li>
+                        <li><a href="#" class="link">create user</a></li>
+                        <li><a href="#" class="link">Terms of use</a></li>
+                        <li><a href="#" class="link">Privacy statement</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Contact Us</h5>
+                    <ul class="mb-0 list-style-none">
+                        <li>Karenslyst Allé 56, 0277 Oslo</li>
+                        <li>kundeservice@presisutleie.no</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-inner-below">
+                <p class="mb-0">Powered by Garantihuset AS</p>
+                <p class="copyright-text">Copyright <span id="current-year"></span> Precise Rentals</p>
+            </div>
+        </footer>
+    </body>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.5.0.min.js"></script>
+    <script src="js/lightgallery-all.min.js"></script>
+    <script src="js/main.js"></script>
+        <script>
+            $(function() {
+                /*footer*/
+                var currentYear = (new Date).getFullYear();
+                $('.footer #current-year').text(currentYear);
+                /*footer*/
+
+                /*header view, if user not at page top*/
+                if ($(window).scrollTop() != 0) {
+                    $('.header-navbar').addClass('bg-white light-box-shadow');
+                    if ($(window).width() >= 992) {
+                        $('.sign-in-btn').css({'border': '2px solid #48a955', 'color': '#48a955'});
+                    }
+                    $('.header-navbar .navbar-links .nav-link').css({'color': '#000', 'text-shadow': 'unset'});
+                    $('.header-navbar .navbar-brand .feelag-logo').css('filter', 'invert(1)');
+                }
+                /*header view, if user not at page top*/
+
+                $(window).on('scroll', function () {
+                    /*header view on scroll*/
+                    $('.header-navbar').addClass('bg-white light-box-shadow');
+                    if ($(window).width() >= 992) {
+                        $('.sign-in-btn').css({'border': '2px solid #48a955', 'color': '#48a955'});
+                        if ($(window).scrollTop() == 0) {
+                            $('.sign-in-btn').css({'border': '2px solid #FFF', 'color': '#FFF'});
+                        }
+                    }
+                    $('.header-navbar .navbar-links .nav-link').css({'color': '#000', 'text-shadow': 'unset'});
+                    $('.header-navbar .navbar-brand .feelag-logo').css('filter', 'invert(1)');
+                    if ($(window).scrollTop() == 0) {
+                        $('.header-navbar').removeClass('bg-white light-box-shadow');
+                        $('.header-navbar .navbar-links .nav-link').css({'color': '', 'text-shadow': ''});
+                        $('.header-navbar .navbar-brand .feelag-logo').css('filter', '');
+                    }
+                    /*header view on scroll*/
+                });
+
+                if ($(window).width() >= 992) {
+                    /*header navbar link hover effect*/
+                    $(".header-navbar .navbar-nav .nav-item.dropdown").hover(function () {
+                        $(this).addClass("show");
+                        $(this).find(".dropdown-menu").addClass("show");
+                    }, function () {
+                        $(this).removeClass("show");
+                        $(this).find(".dropdown-menu").removeClass("show");
+                    });
+                    /*header navbar link hover effect*/
+                }
+
+                $(window).resize(function () {
+                    if ($(window).width() >= 992) {
+                        /*header navbar link hover effect*/
+                        $(".header-navbar .navbar-nav .nav-item.dropdown").hover(function () {
+                            $(this).addClass("show");
+                            $(this).find(".dropdown-menu").addClass("show");
+                        }, function () {
+                            $(this).removeClass("show");
+                            $(this).find(".dropdown-menu").removeClass("show");
+                        });
+                        /*header navbar link hover effect*/
+                    }
+                });
+            });
+        </script>
+</html>
---
+++ common/header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Felag-360</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="images/header/presis-logo-white.svg">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+        <link rel="stylesheet" href="css/bootstrap-4.5.3.min.css">
+        <link rel="stylesheet" href="css/poppins.css">
+<!--        <link rel="stylesheet" href="css/open-sans.css">-->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/lightgallery.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <!--paste this code under the head tag-->
+    <div id="loader">
+        <span id="loaderGif"></span>
+    </div>
+    <!--paste this code under the head tag-->
+    <body>
+        <nav class="navbar navbar-expand-lg header-navbar">
+            <div class="container">
+                <div class="d-flex align-items-center justify-content-between navbar-brand-toggle-outer main-nav-bar">
+                    <a class="mr-0 navbar-brand" href="./">
+<!--                        <img src="images/header/presis-logo-white.svg" class="feelag-logo">-->
+                        <span class="felag-logo-text">Logo</span>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#felagNavBar" aria-controls="felagNavBar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse justify-content-around navbar-links" id="felagNavBar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="dropdownNavOne" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Precise Management System<span class="ml-2 fa fa-angle-down"></span>
+                            </a>
+                            <div class="dropdown-menu animated-dropdown slideIn" aria-labelledby="dropdownNavOne">
+                                <div class="row">
+                                    <div class="col-md-4 dropdown-menu-section">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                    <div class="col-md-4 dropdown-menu-section">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                    <div class="col-md-4 dropdown-menu-section">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./precise-rent-contract.php">Precise Rent Contract</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Prices</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="dropdownNavTwo" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Contact Support<span class="ml-2 fa fa-angle-down"></span>
+                            </a>
+                            <div class="dropdown-menu animated-dropdown slideIn" aria-labelledby="dropdownNavTwo">
+                                <a class="dropdown-item" href="#">Contact Us</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="d-lg-none d-block mt-4 mb-2" align="center">
+                        <a href="#" class="mr-2 btn sign-in-btn">Sign in</a>
+                        <a href="#" class="btn btn-bg">Start now</a>
+                    </div>
+                </div>
+                <div class="d-lg-inline-block d-none">
+                    <a href="#" class="mr-2 btn sign-in-btn">Sign in</a>
+                    <a href="#" class="btn btn-bg">Start now</a>
+                </div>
+            </div>
+        </nav>
