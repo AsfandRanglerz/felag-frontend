6c391699f21776d08c71b8b0fc29b16a561a9b10
AsfandRanglerz <asfand.ranglerz@gmail.com>
2021-09-11 01:06:39 -0700

    changes

--- common/admin-panel-footer.php
+++ common/admin-panel-footer.php
@@ -2,9 +2,19 @@
 
         </footer>
     </body>
-<script src="js/jquery-3.5.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap-4.5.0.min.js"></script>
-<script src="plugins/jquery.steps-1.1.0/jquery.steps.min.js"></script>
-<script src="js/main.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.5.0.min.js"></script>
+    <script src="plugins/jquery.steps-1.1.0/jquery.steps.min.js"></script>
+    <script src="js/main.js"></script>
+    <script lang="js">
+        $(function () {
+        $("#example-form").steps({
+            headerTag: "h6",
+            bodyTag: "section",
+            transitionEffect: "fade",
+            titleTemplate: '<span class="step">#index#</span> #title#'
+            });
+        })
+    </script>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(function() {
-    $("#example-form").steps({
-        headerTag: "h6",
-        bodyTag: "section",
-        transitionEffect: "fade",
-        titleTemplate: '<span class="step">#index#</span> #title#'
-    });
-
     /*wow animation*/
     wow = new WOW(
         {
